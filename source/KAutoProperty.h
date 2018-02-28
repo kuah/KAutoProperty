d96edf23ba265918b512cbe10016cc11182e680a
kuah <284766710@qq.com>
2018-02-28 14:30:26 +0800

    add header

---
+++ source/KAutoProperty.h
@@ -0,0 +1,15 @@
+//
+//  KAutoProperty.h
+//  KAutoProperty
+//
+//  Created by 陈世翰 on 2018/2/28.
+//  Copyright © 2018年 Kuah. All rights reserved.
+//
+
+#if __has_include(<KAutoProperty/KAutoProperty.h>)
+#import <KAutoProperty/NSObject+KuahAutoProperty.h>
+#else
+#import "NSObject+KuahAutoProperty.h"
+#endif
+
+
